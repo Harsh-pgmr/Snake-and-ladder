398bf7d45d057a1bc8aebb74d36b7fe4854dc57a
Harsh <73747616+Harsh-pgmr@users.noreply.github.com>
2020-11-27 10:12:48 +0530

    Initializing to Zero

---
+++ Sanakerladder.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+clear
+
+#Problem Statement: This problem simulates the Snake and Ladder game
+#Author: Harshad BB
+#Date: 27-11-2020
+
+echo "Welcome to Snake and ladder"
+START_POSITION=0
+
+sleep 1
